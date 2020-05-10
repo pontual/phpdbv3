7b6b51c663f7723d23077d30c05aa0da77350dfb
Heitor Chang <heitorchang@gmail.com>
2020-05-10 18:57:12 -0300

    increase cv

--- install.php
+++ install.php
@@ -14,7 +14,7 @@ echo $echo_info . "v3_categoriadeproduto\n";
 $dbh->exec("CREATE TABLE IF NOT EXISTS `v3_categoriadeproduto` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `nome` varchar(255) NOT NULL, `detalhes` text, `publico` bool NOT NULL)");
 
 echo $echo_info . "v3_produto\n";
-$dbh->exec("CREATE TABLE IF NOT EXISTS `v3_produto` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `codigo` varchar(63) NOT NULL UNIQUE, `nome` varchar(255) NOT NULL, `normalizado` varchar(255), `detalhes` text, `sufixo_jpg` varchar(31), `peso` varchar(63), `medidas` varchar(63), `cv` varchar(63), `cv_atualizado` datetime(0), `disponivel_ptl` integer, `reservado_ptl` integer, `disponivel_uni` integer, `reservado_uni` integer, `caixa` integer, `publico` bool NOT NULL)");
+$dbh->exec("CREATE TABLE IF NOT EXISTS `v3_produto` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `codigo` varchar(63) NOT NULL UNIQUE, `nome` varchar(255) NOT NULL, `normalizado` varchar(255), `detalhes` text, `sufixo_jpg` varchar(31), `peso` varchar(63), `medidas` varchar(63), `cv` varchar(255), `cv_atualizado` datetime(0), `disponivel_ptl` integer, `reservado_ptl` integer, `disponivel_uni` integer, `reservado_uni` integer, `caixa` integer, `publico` bool NOT NULL)");
 
 echo $echo_info . "v3_produto_categorias\n";
 $dbh->exec("CREATE TABLE IF NOT EXISTS `v3_produto_categorias` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `produto_id` integer NOT NULL, `categoriadeproduto_id` integer NOT NULL)");
