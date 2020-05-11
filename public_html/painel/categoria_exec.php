aaa5d1091bb82a263e7a7a0ed929a2a6cfb23edc
Heitor Chang <heitorchang@gmail.com>
2020-05-11 09:28:26 -0300

    edit categoria

--- public_html/painel/categoria_exec.php
+++ public_html/painel/categoria_exec.php
@@ -1,37 +1,34 @@
 <?php
 require("../../db_cls.php");
 
-$crud_action = $_POST['crud_action'];
-
-echo $crud_action;
 $dbh = getdbh();
 
-if ($crud_action == "create") {
+$crud_action = $_POST['crud_action'];
+$categoria_id = (int) ($_POST['categoria_id'] ?? "0");
 
-    $nome = $_POST['nome'];
-    $detalhes = $_POST['detalhes'] ?? "";
-    $publico = $_POST['publico'] ?? "0";
 
-    $publico = (int) $publico;
-    
+if ($crud_action == "create") {    
     echo "Insert $nome, $detalhes";
+
     $sql = "insert into v3_categoriadeproduto (nome, detalhes, publico) values (:nome, :detalhes, :publico)";
+    $sql_arguments = [':nome' => $_POST['nome'],
+                      ':detalhes' => $_POST['detalhes'],
+                      ':publico' => (int) ($_POST['publico'] ?? "0")];
     
-    $sth = $dbh->prepare($sql);
-
-    try {
-        $sth->execute([':nome' => $nome,
-                       ':detalhes' => $detalhes,
-                       ':publico' => $publico]);
-    } catch (Exception $e) {
-        echo $e->getMessage();
-    }
-
 } elseif ($crud_action == "update") {
-    echo "Update nome";
-} else {
-    echo "Ação desconhecida: $crud_action";
+    echo "Update $categoria_id";
+
+    $sql = "update v3_categoriadeproduto set nome = :nome, detalhes = :detalhes, publico = :publico where id = :id";
+    $sql_arguments = [':nome' => $_POST['nome'],
+                      ':detalhes' => $_POST['detalhes'],
+                      ':publico' => (int) ($_POST['publico'] ?? "0"),
+                      ':id' => $categoria_id];
 }
 
+$sth = $dbh->prepare($sql);
 
-
+try {
+    $sth->execute($sql_arguments);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
--- public_html/painel/categoria_form.php
+++ public_html/painel/categoria_form.php
@@ -5,31 +5,55 @@
     </head>
     <body>        
         <?php
-        $crud_action = $_GET['crud_action'] ?? 'create';
+        require("../../db_cls.php");
+        
+        $categoria_id = (int) ($_GET['categoria_id'] ?? '0');
+
+        if ($categoria_id != 0) {
+            $crud_action = 'update';
+            $dbh = getdbh();
+            $sql = "select nome, detalhes, publico from v3_categoriadeproduto where id = :id";
+            $sth = $dbh->prepare($sql);
+            $sth->execute([":id" => $categoria_id]);
+
+            $row = $sth->fetch();
+            $nome = $row['nome'];
+            $detalhes = $row['detalhes'];
+        } else {
+            $crud_action = 'create';
+            $nome = "";
+            $detalhes = "";
+        }
             
+
         ?>
         <div class="container">
 
             <form method="post" action="categoria_exec.php" class="pure-form pure-form-aligned">
 	        <input type="hidden" name="crud_action" value="<?= $crud_action ?>">
+	        <input type="hidden" name="categoria_id" value="<?= $categoria_id ?>">
                 <fieldset>
                     <div class="field">
                         <label class="label">Nome</label>
                         <div class="control">
-                            <input class="input" name="nome">
+                            <input class="input" name="nome" value="<?= $nome ?>">
                         </div>
                     </div>
 
                     <div class="field">
                         <label class="label">Informações adicionais</label>
                         <div class="control">
-                            <textarea class="textrea" name="detalhes" rows="4" cols="50"></textarea>
+                            <textarea class="textrea" name="detalhes" rows="4" cols="50"><?=$detalhes ?></textarea>
                         </div>
                     </div>
 
                     <div class="field">
 	                <label class="checkbox">
-	                    <input type="checkbox" name="publico" value="1">
+	                    <input type="checkbox"
+                                   name="publico"
+                                   value="1"
+                                   <?= $row['publico'] == 1 ? "checked" : "" ?>
+                            >
 	                    Visível no site
 	                </label>
 	            </div>
