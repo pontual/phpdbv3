ab5d50b4b306d0e072a63c703597a8a43dd02c9c
Heitor Chang <heitorchang@gmail.com>
2020-05-11 15:14:11 -0300

    produto list group concat cats

--- public_html/painel/produto_list.php
+++ public_html/painel/produto_list.php
@@ -3,11 +3,11 @@ require("../../db_cls.php");
 
 $dbh = getdbh();
 
-$sql = "select p.id, p.codigo, p.nome from v3_produto as p"; // inner join v3_produto_categoria as pc on p.id = pc.produto_id inner join v3_categoriadeproduto as c on c.id = pc.categoriadeproduto_id";
+$sql = "select p.id, p.codigo, p.nome, GROUP_CONCAT(c.nome order by c.nome separator ', ') as cats from v3_categoriadeproduto as c inner join v3_produto_categoria as pc on c.id = pc.categoriadeproduto_id inner join v3_produto as p on p.id = pc.produto_id group by p.id order by p.id";
 
 foreach ($dbh->query($sql) as $row) {
 ?>
-    <a href="produto_form.php?produto_id=<?= $row['id'] ?>"><?= $row['codigo'] ?></a><br>
+    <a href="produto_form.php?produto_id=<?= $row['id'] ?>"><?= $row['codigo'] ?></a> <?= $row['cats'] ?><br>
 <?php 
 }
 ?>
